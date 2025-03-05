bfe03afa912050c1bc11a34fa00db3e3b60a9cad
tim <timmcsweeney12@hotmail.com>
2025-03-05 17:28:21 +0000

    tictactoegame

---
+++ src/components/creater.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Creater() {
+  return (
+    <>
+    <div className='container-fluid texr-center p-4 bg-light rounded shadow mt-5'>
+
+    <h1>Created by Tim Mc Sweeney</h1>
+    <h1>Student Number: 23253180</h1>
+
+    </div>
+  
+
+    </>
+
+  )
+}
+
+export default Creater
---
+++ src/components/navbar.jsx
@@ -0,0 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Creater from './creater';
+import "./navbar.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function Navbar() {
+  return (
+    <nav className="custom-navbar">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-navigation"
+        >
+          <span className=""></span>
+        </button>
+        <div className="" id="main-navigation">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Tic-Tac-Toe</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/creater">Creater</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
